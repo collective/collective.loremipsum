f3393c700cac2dec03aad34e54c8abfcb179bcb9
JC Brand <jc@opkode.com>
2011-12-08 17:31:59 +0200

    Bugfix. README.txt has been renamed to README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1'
 setup(name='collective.loremipsum',
       version=version,
       description="Creates dummy content with populated Lorem Ipsum from loripum.net",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
