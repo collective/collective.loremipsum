04ae661dcad1291faede3ca561ec8cd721e1187f
JC Brand <jc@opkode.com>
2014-04-29 01:30:06 +0200

    Add plone.api as dependency

--- setup.py
+++ setup.py
@@ -3,35 +3,37 @@ import os
 
 version = '0.9.dev0'
 
-setup(name='collective.loremipsum',
-      version=version,
-      description="Creates dummy content with populated Lorem Ipsum.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='collective.loremipsum',
+    version=version,
+    description="Creates dummy content with populated Lorem Ipsum.",
+    long_description=open("README.rst").read() + "\n" +
+                open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone :: 4.1",
         "Programming Language :: Python",
-        ],
-      keywords='dummy data',
-      author='JC Brand',
-      author_email='jc@opkode.com',
-      url='https://github.com/collective/collective.loremipsum',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'loremipsum',
-          'Products.CMFPlone',
-          'plone.dexterity',
-          'plone.namedfile',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='dummy data',
+    author='JC Brand',
+    author_email='jc@opkode.com',
+    url='https://github.com/collective/collective.loremipsum',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'loremipsum',
+        'Products.CMFPlone',
+        'plone.dexterity',
+        'plone.namedfile',
+        'plone.api',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+        """,
+    )
